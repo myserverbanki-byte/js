0d9553711659b137a8c2d5bf4ae58ed4d8c626f0
myserverbanki-byte <myserverbanki@gmail.com>
2026-02-10 13:25:20 +0300

    3

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     console.log('Hello World');
+    res.end('Hello World');
 }
 
 );
-server.listen(3000)
+server.listen(3001)
 console.log('Server Started');
