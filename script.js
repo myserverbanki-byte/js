2120fc09b7ffe9fb285825d3ad0cdee1e45f1b9e
myserverbanki-byte <myserverbanki@gmail.com>
2026-02-20 16:15:09 +0300

    1

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const fs = require('fs');
 const text = fs.readFileSync('script.js', 'utf-8');
 console.log(text);
+class Button {
+    constructor(text) {
+        this.text = text
+        this.render()
+
+    }
+    render() {
+        const placeTorender = document.querySelector('.btns')
+        if (placeTorender) {
+            const btn = document.createElement('button')
+            btn.classList.add('btn')
+            btn.innerHTML = this.text
+            placeTorender.appendChild(btn)
+
+        }
+    }
+}
+new Button('click me');
