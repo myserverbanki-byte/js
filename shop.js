c9f60db8aa926b2c8b568afa590ac5b66c77628c
myserverbanki-byte <myserverbanki@gmail.com>
2026-02-21 12:21:24 +0300

    1

---
+++ shop.js
@@ -0,0 +1,43 @@
+class List {
+    items = []
+    constructor() {
+        let goods = this.fetchGoods()
+        goods = goods.map((cur) => {
+            return new GoodItem(cur)
+        })
+        this.items.push(...goods)
+    }
+    fetchGoods() {
+        return [
+            { name: 'Shirt', price: 150 },
+            { name: 'Socsk', price: 100 },
+            { name: 'Jocsk', price: 250 },
+            { name: 'Shoes', price: 350 }
+
+        ]
+    }
+    render() {
+        this.items.forEach(good => {
+            good.render()
+        })
+    }
+}
+
+class GoodItem {
+    name = ''
+    price = 0
+    constructor({ name, price }) {
+        this.name = name
+        this.price - price
+
+    }
+    render() {
+        const placeToRender = document.querySelector('.goods-list')
+        if (placeToRender) {
+            const block = document.createElement(';div')
+            block.innerHTML = `Товар : ${this.name} = ${this.price}`
+            placeToRender.appendChild(block)
+        }
+    }
+}
+const List = new List()
